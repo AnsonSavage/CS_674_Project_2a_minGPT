450704149c31f238d70cee90b769948769d5f355
AnsonSavage <ansonsavage@gmail.com>
2024-11-07 10:44:33 -0700

    Create loglikelihood and loglikelihood_rolling methods!

--- lm_eval/models/min_gpt.py
+++ lm_eval/models/min_gpt.py
@@ -1,6 +1,7 @@
 import random
 from typing import List, Tuple
 import torch
+import torch.nn.functional as F
 
 import sys
 sys.path.append('/home/ansonsav/cs_674/project_2a_minGPT/minGPT/mingpt')
@@ -24,6 +25,8 @@ class MinGPTEval(LM):
         self.tokenizer = tokenizer
     
     def generate_until(self, requests: list) -> List[str]:
+        # Each item in requests has a .args which is : Tuple[str, dict]
+        # The first string is the prompt and the second dict is the parameters
         print(requests)
         total_output = []
 
@@ -48,15 +51,67 @@ class MinGPTEval(LM):
         return tokens
     
     def loglikelihood(self, requests) -> List[Tuple[float | bool]]:
+        # Each item in requests has a .args which is : Tuple[str, str]
+        # The first string is the context and the second string is the expected output
+        
+        # Pretty print the requests
+        response = []
+
         for request in requests:
-            context, expected_output = request.args
-            context_tokens = self.tokenize(context)
-            expected_output_tokens = self.tokenize(expected_output)
-            generated_tokens = []
-            while len(generated_tokens) < len(expected_output_tokens):
-                next_token = self.model.generate_next_token(context_tokens)
-                generated_tokens.append(next_token)
-                context_tokens = torch.cat((context_tokens, next_token), dim=1)
+            with torch.no_grad():
+                context, expected_output = request.args
+                context_tokens = self.tokenize(context)
+                expected_output_tokens = self.tokenize(expected_output)
+                total_context = torch.cat((context_tokens, expected_output_tokens), dim=1)
+
+                total_input = total_context[:, :-1] # Remove the last token
+
+                generated_output_logits, _ = self.model(total_input)
+                print(generated_output_logits.shape)
+                generated_output_logits = generated_output_logits[:, -len(expected_output_tokens[0]):] # Only take the logits corresponding to the expected output
+                print(generated_output_logits.shape)
+                assert generated_output_logits.shape[2] == self.tokenizer.vocab_size, "The number of logits should be equal to the vocab size"
+
+                # We now have to figure out which of these logits correspond to the expected output
+
+                log_likelihoods_of_generated_output_logits = F.log_softmax(generated_output_logits, dim=2)
+
+                # Now we need to index into this with the expected_output_tokens
+                # We need to get the log likelihood of the expected_output_tokens and then sum them up
+                # We can do this by using the gather method
+                # The gather method takes the indices of the elements to gather from the tensor
+                # The first argument is the dimension to gather from
+                # The second argument is the indices to gather
+                # The third argument is the dimension to gather into
+                print(log_likelihoods_of_generated_output_logits.shape)
+                print(expected_output_tokens.shape)
+                log_likelihoods_of_expected_output = torch.gather(log_likelihoods_of_generated_output_logits, 2, expected_output_tokens.unsqueeze(2)).squeeze(2) # We unsqueeze the expected_output_tokens so that the number of dimensions between the input and the index remains the same
+                # If it was the greedy response, then the argmax along the last dimension of the log_likelihoods_of_generated_output_logits would be the expected_output_tokens
+                is_greedy = torch.equal(log_likelihoods_of_generated_output_logits.argmax(dim=2), expected_output_tokens)
+                # Because these are log_likelihoods, then the sum of them is the log likelihood of the expected output (if they weren't logged, it'd be the product)
+                response.append((log_likelihoods_of_expected_output.sum().item(), is_greedy))
+        
+        return response
+
+
+
 
     def loglikelihood_rolling(self, requests) -> List[float]:
-        pass
+        # Each item in requests has a .args which is : Tuple[str,]
+        # The string is the input string to the model whose entire loglikelihood, conditioned purely on the EOT token, will be calculated
+        response = []
+        eot_token = torch.tensor(self.tokenizer.encode("<|endoftext|>"), dtype=torch.long).unsqueeze(0)
+        for request in requests:
+            with torch.no_grad():
+                input_string = request.args[0]
+                expected_tokens = self.tokenize(input_string)
+                input_tokens = torch.cat((eot_token, expected_tokens), dim=1) # Prepend the EOT token
+                input_tokens = input_tokens[:, :-1] # Remove the last token so that our model can predict it
+                logits = self.model(input_tokens)
+                assert logits.shape[2] == self.tokenizer.vocab_size, "The number of logits should be equal to the vocab size"
+                assert logits.shape[1] == len(expected_tokens[0]), "The number of logit sets should be equal to the number of tokens in the expected output"
+
+                log_likelihoods_of_logits = F.log_softmax(logits, dim=2)
+                log_likelihoods_of_expected_output = torch.gather(log_likelihoods_of_logits, 2, expected_tokens.unsqueeze(2)).squeeze(2)
+                response.append(log_likelihoods_of_expected_output.sum().item())
+        return response
--- projects/project_2a_train_on_jsonl_files/evaluate_model.py
+++ projects/project_2a_train_on_jsonl_files/evaluate_model.py
@@ -1,4 +1,4 @@
-# from lm_eval import evaluator
+from lm_eval import evaluator
 import torch
 import sys
 sys.path.append('/home/ansonsav/cs_674/project_2a_minGPT/minGPT')
@@ -44,31 +44,31 @@ def get_config():
 
     return C
 
-if __name__ == '__main__':
-    # get the config
-    config = get_config()
+# if __name__ == '__main__':
+#     # get the config
+#     config = get_config()
 
-    setup_logging(config)
-    set_seed(config.system.seed)
+#     setup_logging(config)
+#     set_seed(config.system.seed)
 
-    # create the data
-    dataset = GPT2TokenizedDataset(JSONLDataset(config.data_path, length=10000)) # length=None means use all the data
+#     # create the data
+#     dataset = GPT2TokenizedDataset(JSONLDataset(config.data_path, length=10000)) # length=None means use all the data
 
-    config.model.vocab_size = dataset.get_vocab_size()
-    config.model.block_size = dataset.get_block_size()
+#     config.model.vocab_size = dataset.get_vocab_size()
+#     config.model.block_size = dataset.get_block_size()
 
-    # create the model
-    model = GPT(config.model)
-    checkpoint = torch.load("/home/ansonsav/cs_674/project_2a_minGPT/minGPT/projects/project_2a_train_on_jsonl_files/out/project_2a_train_on_jsonl_files/iteration_3/checkpoint_1960000.pth")
-    model.load_state_dict(checkpoint['model'])
-    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    # input_tokens = torch.tensor(tokenizer("The quick brown fox", truncation=True, max_length=1024)['input_ids'], dtype=torch.long).unsqueeze(0)
-    # output_tokens = model.generate(input_tokens, 100)
-    # output_text = tokenizer.decode(output_tokens[0])
-    # print(output_text)
-    evaluator = MinGPTEval(model, tokenizer)
-    results = evaluator.generate_until([DummyInstance(("The quick brown fox", {"until": [".", "\n\n"], "max_gen_toks": 200}))])
-    print(results)
+#     # create the model
+#     model = GPT(config.model)
+#     checkpoint = torch.load("/home/ansonsav/cs_674/project_2a_minGPT/minGPT/projects/project_2a_train_on_jsonl_files/out/project_2a_train_on_jsonl_files/iteration_3/checkpoint_1960000.pth")
+#     model.load_state_dict(checkpoint['model'])
+#     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+#     # input_tokens = torch.tensor(tokenizer("The quick brown fox", truncation=True, max_length=1024)['input_ids'], dtype=torch.long).unsqueeze(0)
+#     # output_tokens = model.generate(input_tokens, 100)
+#     # output_text = tokenizer.decode(output_tokens[0])
+#     # print(output_text)
+#     tester = MinGPTEval(model, tokenizer)
+#     results = tester.generate_until([DummyInstance(("The quick brown fox", {"until": [".", "\n\n"], "max_gen_toks": 200}))])
+#     print(results)
 
 
 
@@ -97,6 +97,6 @@ def evaluate_mingpt_hellaswag(model_path):
     )
     return results['results']['hellaswag']['acc,none']
 
-# score = evaluate_mingpt_hellaswag("/home/ansonsav/cs_674/project_2a_minGPT/minGPT/projects/project_2a_train_on_jsonl_files/out/project_2a_train_on_jsonl_files/iteration_3/checkpoint_1960000.pth")
-# print(score)
+score = evaluate_mingpt_hellaswag("/home/ansonsav/cs_674/project_2a_minGPT/minGPT/projects/project_2a_train_on_jsonl_files/out/project_2a_train_on_jsonl_files/iteration_3/checkpoint_1960000.pth")
+print(score)
 
