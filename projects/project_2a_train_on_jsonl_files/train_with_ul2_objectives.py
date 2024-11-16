583f31224040603eacc7036995ea457f885715e9
AnsonSavage <ansonsavage@gmail.com>
2024-11-15 17:19:04 -0700

    Add UL2 denoising datasets and add span corruption code

--- projects/project_2a_train_on_jsonl_files/train_with_ul2_objectives.py
+++ projects/project_2a_train_on_jsonl_files/train_with_ul2_objectives.py
@@ -2,6 +2,7 @@ from jsonl_dataset import JSONLDataset
 from gpt_2_tokenized_dataset import GPT2TokenizedDataset
 import os
 import matplotlib.pyplot as plt
+from ul2_datasets import RegularDenoisingDataset, ExtremeDenoisingDataset, SequentialDenoising
 from transformers import GPT2Tokenizer
 
 
@@ -52,45 +53,55 @@ if __name__ == '__main__':
     set_seed(config.system.seed)
 
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    # Add the mode switching tokens, 
-    
-    dataset = GPT2TokenizedDataset(JSONLDataset(config.data_path, length=None), tokenizer) # length=None means use all the data
 
-    config.model.vocab_size = dataset.get_vocab_size()
-    config.model.block_size = dataset.get_block_size()
+    # Add the mode switching tokens, [NLU], [NLG], and [S2S] for regular denoising, extreme denoising, and sequential denoising respectively
+    tokenizer.add_tokens(['[NLU]', '[NLG]', '[S2S]'], special_tokens=True) # TODO: use add_special_tokens if this doesn't
+
+    # Add the suffix token
+    tokenizer.add_tokens(['[SUFFIX]'], special_tokens=True)
+
+    # The end token will just use '<|endoftext|>'
+
+    # Add sentinel tokens, <extra_id_0> through <extra_id_99>
+    tokenizer.add_tokens([f'<extra_id_{i}>' for i in range(100)]) # TODO: do they need to be special tokens?
+    
+    tokenized_dataset = GPT2TokenizedDataset(JSONLDataset(config.data_path, length=None), tokenizer) # length=None means use all the data
+    # TODO: Create the denoising datasets
+    regular_denoising_dataset = RegularDenoisingDataset(tokenized_dataset)
+
+    # TODO: is the attention mask configured in the tokenizer?
+
+    config.model.vocab_size = tokenized_dataset.get_vocab_size()
+    config.model.block_size = tokenized_dataset.get_block_size()
 
     # create the model
     model = GPT(config.model)
 
     # create the trainer
-    trainer = Trainer(config.trainer, model, dataset)
+    trainer = Trainer(config.trainer, model, tokenized_dataset)
 
     def batch_end_callback(trainer: Trainer):
         print_every_n = 100
-        save_checkpoint_every_n = 10000
         evaluate_every_n = 10000
+        num_ul2_objective_iterations = evaluate_every_n // 100 # 1% of the iterations
         if trainer.iter_num % print_every_n == 0:
             print(f'iteration: {trainer.iter_num}', flush=True)
         
-        if trainer.iter_num % save_checkpoint_every_n == 0:
-            trainer.checkpoint(os.path.join(config.system.work_dir, f'checkpoint_{trainer.iter_num}.pth'))
-        
         if trainer.iter_num % evaluate_every_n == 0:
-            # Evaluate the model
-            print(trainer.loss_history[-1], flush=True)
+            # Checkpoint the model trained using causal language modeling
+            trainer.checkpoint(os.path.join(config.system.work_dir, f'checkpoint_{trainer.iter_num}.pth'))
+
+            # Evaluate the model using the UL2 objectives
+            for _ in range(num_ul2_objective_iterations):
+                # TODO: train the model using the denoising datasets
+                # Train with sequential 50% of the time, extreme 25% of the time, and regular 25% of the time
+                pass
             
-            try:
-                # Create a graph of the loss history
-                plt.plot(trainer.loss_history)
-                plt.xlabel('Iteration')
-                plt.ylabel('Loss')
-                plt.title('Loss history')
-                plt.savefig(os.path.join(config.system.work_dir, f'loss_history_iteration_{trainer.iter_num}.png'))
-                plt.clf()
-            except Exception as e:
-                print(f'Error creating loss history graph: {e}')
+            # TODO: Save the model checkpoint after training with the UL2 objectives
 
-
+            # Revert back to training with causal language modeling
+            # Load the model checkpoint saved with causal language modeling
+            # continue
 
     trainer.set_callback('on_batch_end', batch_end_callback)
     trainer.run()
--- projects/project_2a_train_on_jsonl_files/ul2_datasets.py
+++ projects/project_2a_train_on_jsonl_files/ul2_datasets.py
@@ -12,15 +12,92 @@ class DenoisingDataset(Dataset, ABC):
     """
     def __init__(self, tokenized_dataset: GPT2TokenizedDataset, mode_token_string):
         self.tokenized_dataset = tokenized_dataset
-        self.mode_token = mode_token_string
-    
+        self.tokenizer = tokenized_dataset.tokenizer
+        
+        # Get the mode token
+        self.mode_token = self.tokenizer.encode(mode_token_string)
+        assert len(self.mode_token) == 1
+        self.mode_token = torch.tensor(self.mode_token)
+
+        # Set the suffix and end tokens
+        self.suffix_token = torch.tensor(self.tokenizer.encode('[SUFFIX]'))
+        self.end_token = torch.tensor(self.tokenizer.encode('<|endoftext|>'))
+
     def __len__(self):
         return len(self.tokenized_dataset)
     
     def __getitem__(self, idx):
         tokens = self.tokenized_dataset[idx]
-        # TODO: prepend the mode token to the tokens
+        tokens = self._prepend_mode_token(tokens)
+        
+        # TODO: what is the prefix and what is the suffix?
         return self._noise(tokens)
+
+    def create_prefix_and_suffix(self, corrupted_tokens, corrupted_spans, removed_tokens):
+        """ Creates the prefix and suffix for the denoising task
+        Args:
+        Returns:
+        """
+        
+        # Create the prefix
+        train_tensor = None
+        assert len(corrupted_spans) == len(removed_tokens)
+        for i in range(len(corrupted_spans)):
+            extra_id_token = torch.tensor(self.tokenizer.encode(f'<extra_id_{i}>'))
+            corrupted_span_start, corrupted_span_end = corrupted_spans[i]
+
+            # If we're on the first iteration, we want the train tensor to go from the start of the tokens to the start of the corrupted span
+            if train_tensor is None:
+                assert i == 0
+                train_tensor = corrupted_tokens[:corrupted_span_start].clone() # TODO: is cloning necessary?
+            else:
+                previous_corrupted_span_start, previous_corrupted_span_end = corrupted_spans[i - 1]
+                train_tensor = torch.cat((train_tensor, corrupted_tokens[previous_corrupted_span_end:corrupted_span_start].clone()))
+            train_tensor = torch.cat((train_tensor, extra_id_token))
+        
+        # Create the suffix
+        train_tensor = torch.cat((train_tensor, self.suffix_token)) # add the suffix token
+        for i in range(len(removed_tokens)):
+            removed_token = removed_tokens[i]
+            extra_id_token = torch.tensor(self.tokenizer.encode(f'<extra_id_{i}>'))
+            train_tensor = torch.cat((train_tensor, extra_id_token, removed_token))
+
+        # Add the end token
+        train_tensor = torch.cat((train_tensor, self.end_token))
+        
+
+
+
+    def corrupt(self, tokens, min_span_size: int, max_span_size: int, percentage_to_corrupt: float) -> tuple:
+        """ Corrupts a percentage of the tokens with spans of random size between min_span_size and max_span_size. Returns the corrupted tokens, the corresponding spans, and tokens that were removed
+        Args:
+            tokens (torch.Tensor): The tokens to corrupt
+            min_span_size (int): The minimum size of the span to corrupt
+            max_span_size (int): The maximum size of the span to corrupt
+            percentage_to_corrupt (float): The percentage of the tokens to corrupt
+        Returns:
+            tuple: A tuple containing the corrupted tokens, the corrupted spans (a list of tuples representing start and end indices of corruption) and a list of the tokens that were removed
+        """
+        removed_tokens = []
+        corrupted_spans = []
+        percentage_corrupted = 0
+        corruption_token = -1 # A placeholder that represents a corrupted token
+        while percentage_corrupted < percentage_to_corrupt:
+            span_size = torch.randint(min_span_size, max_span_size, (1,)).item()
+            start_index = torch.randint(0, len(tokens) - span_size, (1,)).item()
+            end_index = start_index + span_size
+            if tokens[start_index:end_index].eq(corruption_token).any(): # If the span already contains a corruption token, perform rejection sampling
+                continue
+            removed_tokens.append(tokens[start_index:end_index].clone())
+            corrupted_spans.append((start_index, end_index))
+
+            tokens[start_index:end_index] = corruption_token
+            percentage_corrupted += span_size / len(tokens)
+        return tokens, corrupted_spans, removed_tokens
+
+    
+    def _prepend_mode_token(self, tokens):
+        return torch.cat((self.mode_token, tokens))
 
     @abstractmethod
     def _noise(self, tokens):
@@ -32,14 +109,20 @@ class RegularDenoisingDataset(DenoisingDataset):
     Has a standard span corruption noise model
     It corrupts spans with a uniform mean of 3 and a corruption rate of 15%
     """
-    pass
+    def __init__(self, tokenized_dataset: GPT2TokenizedDataset):
+        super().__init__(tokenized_dataset, '[NLU]')
+    
+    def _noise(self):
+        return self.corrupt(tokens, 1, 5, 0.15)
 
 class ExtremeDenoisingDataset(DenoisingDataset):
     """ Prepends the [NLG] token to the tokens
     """
-    pass
+    def __init__(self, tokenized_dataset: GPT2TokenizedDataset):
+        super().__init__(tokenized_dataset, '[NLG]')
 
 class SequentialDenoising(DenoisingDataset):
     """ Prepends the [S2S] token to the tokens
     """
-    pass
+    def __init__(self, tokenized_dataset: GPT2TokenizedDataset):
+        super().__init__(tokenized_dataset, '[S2S]')
